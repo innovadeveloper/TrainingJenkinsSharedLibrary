d1008b4adeb283ce727d96ca03d714e91a83dde0
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-18 00:20:32 -0500

    modified

---
+++ src/com/utils/JenkinsfileUtil.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+package com.utils
+
+class JenkinsfileUtil {
+//    def steps
+//    def script
+//
+//    JenkinsfileUtil(def steps, def script) {
+//        this.steps = steps
+//        this.script = script
+//    }
+
+    // Ejemplo de acceso a variables definidas en el script del pipeline
+    static def getProjectName() {
+        return "azar"
+    }
+}
