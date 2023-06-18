4c87d95af020180be043202bb81bbac318ca6a26
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-18 17:41:16 -0500

    modified

---
+++ vars/webJenkinsFileUtils.groovy
@@ -0,0 +1,12 @@
+//import groovy.json.JsonSlurper
+//import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
+import java.io.InputStream
+import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
+
+class WebJenkinFileUtils {
+    def pathFile = 'test.txt'
+
+    def main(){
+        return 'hola'
+    }
+}
