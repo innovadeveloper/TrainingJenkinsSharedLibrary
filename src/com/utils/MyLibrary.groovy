f8ab97031bd69bf4072db5c0bfabe05eb2f459c1
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-18 13:16:11 -0500

    modified

--- src/com/utils/MyLibrary.groovy
+++ src/com/utils/MyLibrary.groovy
@@ -1,12 +1,14 @@
-import groovy.json.JsonSlurper
-import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
+//import groovy.json.JsonSlurper
+//import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
 
 class MyLibrary {
     String readResourceFile() {
 //        def resourceStream = getClass().getResourceAsStream("/my-resource-file.txt")
 //        def resourceContent = libraryResource('my-resource-file.txt')
-        def resourceContent = SandboxInvoker.libraryResource('my-resource-file.txt')
+//        def resourceContent = SandboxInvoker.libraryResource('my-resource-file.txt')
+        def resourceStream = getClass().getResourceAsStream('/my-resource-file.txt')
+
 //        def resourceContent = resourceStream?.text
-        return resourceContent
+        return resourceStream
     }
 }
