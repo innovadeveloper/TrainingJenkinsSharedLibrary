cf3a0ac1e5df87586917da67d19e270fa2093031
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-18 02:09:39 -0500

    modified

--- src/com/utils/MyLibrary.groovy
+++ src/com/utils/MyLibrary.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonSlurper
 
 class MyLibrary {
     String readResourceFile() {
-        def resourceStream = getClass().getResourceAsStream("/my-resource-file.txt")
+        def resourceStream = getClass().getResourceAsStream("/resources/my-resource-file.txt")
         def resourceContent = resourceStream?.text
         return resourceContent
     }
