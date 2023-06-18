fe19ad1dabbbd91078fec8ae7166fb6c10e26353
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-18 17:21:19 -0500

    modified

---
+++ vars/MyLibrary.groovy
@@ -0,0 +1,19 @@
+//import groovy.json.JsonSlurper
+//import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
+import java.io.InputStream
+import com.cloudbees.groovy.cps.sandbox.SandboxInvoker
+
+def pathFile = 'test.txt'
+String readAsInputStream() {
+    def myResource = this.class.getResourceAsStream(pathFile)
+    return myResource
+}
+String readAsInputStreamTwo() {
+    def myResource = this.getClass().getClassLoader().getResourceAsStream(pathFile)
+    return myResource
+}
+
+String readAsResource() {
+    def myResource = libraryResource(pathFile)
+    return myResource
+}
